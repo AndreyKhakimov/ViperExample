e8011b77810dac5d5c1ff100c9885c654198ec3b
AndreyKhakimov <khakimov013@gmail.com>
2022-07-28 15:56:29 +0200

    Added CourseListConfigurator

---
+++ SwiftbookApp/CourseList/CourseListConfigurator.swift
@@ -0,0 +1,25 @@
+//
+//  CourseListConfigurator.swift
+//  SwiftbookApp
+//
+//  Created by Andrey Khakimov on 25.07.2022.
+//  Copyright © 2022 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+protocol CourseListConfiguratorInputProtocol {
+    func configure(with viewController: CourseListViewController)
+}
+
+class CourseListConfigurator: CourseListConfiguratorInputProtocol {
+    
+    func configure(with viewController: CourseListViewController) {
+        let presenter = CourseListPresenter(view: viewController)
+        let interactor = CourseListInteractor(presenter: presenter)
+        
+        viewController.presenter = presenter
+        presenter.interactor = interactor
+    }
+    
+}
